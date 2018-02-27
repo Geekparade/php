1026c758e5b83bf1077dc2ce98da33a6fdde5d14
Geekparade <david_da_silva0hotmail.fr>
2018-02-27 14:42:11 +0100

    exo 2 fini

--- variables/exercice2.php
+++ variables/exercice2.php
@@ -2,15 +2,14 @@
 /**
 * Ajouter deux elements a la fin du tableau $number
 * puis afficher le nombre d'élement du tableau ainsi que le 2 eme element.
-* 
 * Resultat:
-* 
 * Nombre d'élement : 6
 * Deuxieme element : 2
-* 
 * http://php.net/manual/fr/ref.array.php
 */
-
-
-
 $number = [1,2,3,4];
+array_push($number, 5, 6);
+$elementNumber = count($number);
+echo "<p> Nombre d'élément : $elementNumber </p>";
+echo "<p> Deuxième élément : " . ($number[1]) . "</p>";
+?>
