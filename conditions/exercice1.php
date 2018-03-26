3386b3db62795453c9d2202093faec1eb6c4baf8
Geekparade <david_da_silva0hotmail.fr>
2018-03-26 15:03:01 +0200

    condition exo 1 fini

--- conditions/exercice1.php
+++ conditions/exercice1.php
@@ -1,16 +1,16 @@
 <?php
-echo '<h1>Completez ce code source pour que le script affiche :';
+echo '<h1>Complétez ce code source pour que le script affiche :';
 echo '<h3>$num1 est :</h3>';
 echo '<ul>
-		<li>inferieur a $num2</li>
-		<li>superieur a $num2</li>
-		<li>egal a $num2</li>
+		<li>inférieur a $num2</li>
+		<li>ou supérieur a $num2</li>
+		<li>ou égal a $num2</li>
 	</ul>';
 
 $num1 = $_GET['num1'];
 $num2 = $_GET['num2'];
 if(!isset($num1) || !isset($num2)) {
-	die("Vous devez appeler le script de cette facon : <br /><br /><strong>http://localhost/formation/PHP/exercices/conditions/exercice1.php?num1=4&num2=3</strong>");
+	die("Vous devez appeler le script de cette facon : <br /><br /><strong>http://localhost/php/conditions/exercice1.php?num1=4&num2=3</strong>");
 }
 
 if ( $num1 < $num2 )
