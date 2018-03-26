0b9e110c324a58f68e29f738700c3b046e080c50
Geekparade <david_da_silva0hotmail.fr>
2018-03-26 11:47:25 +0200

    boucle exo 7 fini

--- boucles/exercice7.php
+++ boucles/exercice7.php
@@ -1,8 +1,31 @@
-<?PHP
-/**
-* Afficher les multiples de 5 de 0 a 100 sans faire de multiplication
-* Le faire avec for
-* Le faire avec while
-*
-*
-*/
+<h1>Afficher les multiples de 5 de 0 a 100 sans faire de multiplication :</h1>
+<ul> 
+	<li>
+		<h3>le faire avec for .</h3>
+		<?php
+		for ( $i = 0 ; $i <= 100 ; $i++ )
+			{
+				if ( $i%5 == 0 )
+				{
+					echo $i . ' ';
+				};
+			};
+		?>
+	</li>
+
+	
+	<li>
+		<h3>le faire avec while .</h3>
+		<?php
+		$j = 0;
+		while ( $j <= 100 ) 
+		{
+			if ( $j%5 == 0)
+			{
+				echo $j . ' ';
+			}
+			$j++;				
+		}
+		?>
+	</li> 	
+</ul> 
