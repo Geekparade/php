d0d21bcde6132694cbefcfeec21f42f4ed9e19d2
Geekparade <david_da_silva0hotmail.fr>
2018-03-26 15:49:00 +0200

    condition exo 3 fini

--- conditions/exercice3.php
+++ conditions/exercice3.php
@@ -1,20 +1,27 @@
 <?php
-/**
-* Completez ce code source pour que le script affiche si un nombre est pair ou impair
-*
-*   $nombre est pair
-*   OU
-*   $nombre  est impair
-*
-*/
+echo '<h1> Completez ce code source pour que le script affiche si un nombre est pair ou impair :</h1>';
+echo '<ul>
+		<li>	   
+			$nombre est pair
+		</li>	
+		OU 
+		<li>	  
+			$nombre  est impair
+		</li>	
+	</ul>';
+echo '<p>AIDE : l\'opérateur modulo "%" permet de connaitre le reste d\'une division </p>';
+
 $nombre = $_GET['nombre'];
 if(is_null($nombre)) {
-	die("Vous devez appeler le script de cette facon : <br /><br /><strong>http://localhost/formation/PHP/exercices/conditions/exercice3.php?nombre=4</strong>");
+	die("Vous devez appeler le script de cette facon : <br /><br /><strong>http://localhost/php/conditions/exercice3.php?nombre=4</strong>");
 }
-/*
-* Completez le code ici
-*
-* AIDE :
-* L'operateur modulo "%" permet de connaitre le reste d'une division
-*/
+// Mon code
+if ( $nombre % 2 )
+{
+	echo $nombre . ' est un nombre impair.';
+}
+else
+{
+	echo $nombre . ' est un nombre pair.';
+}
 
