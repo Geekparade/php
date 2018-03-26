a1f9c4232e5ef523c59ba6f170cd2d010fa6a1df
Geekparade <david_da_silva0hotmail.fr>
2018-03-26 11:02:23 +0200

    boucle exo 5 finis

--- boucles/exercice5.php
+++ boucles/exercice5.php
@@ -1,6 +1,16 @@
+<h1>Creer un script qui affiche les tables de multiplications des nombres de 1 a 5 .</h1>
+<br/>
+<h3>Les tables de multiplications :</h3>
 <?php
+	for ( $i = 1 ; $i <= 5 ; $i++)
+	{
+		echo '<p>Table de multiplication de ' . $i . ' :</p>';
+		for ( $j = 0 ; $j <= 10 ; $j++ )
+		{
+			echo $i . ' X ' . $j . ' = ' . $i*$j . '<br />';
+		};
+	};
 
-/**
-* Creer un script qui affiche les tables de multiplications des nombres de 1 a 5
-*/
+?>
+
 
