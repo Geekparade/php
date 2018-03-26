51ecacbe2e3067bf19b90d05a364a7027a7b601b
Geekparade <david_da_silva0hotmail.fr>
2018-03-26 15:59:28 +0200

    condition exo 4 fini

--- conditions/exercice4.php
+++ conditions/exercice4.php
@@ -1,19 +1,12 @@
 <?php
-/**
-* Completez ce code source pour que le script affiche
-*
-*   $numX est le plus grand des trois nombres
-*
-*/
+echo '<h1>Completez ce code source pour que le script affiche \'$numX est le plus grand des trois nombres\'.</h1>';
+
 $num1 = $_GET['num1'];
 $num2 = $_GET['num2'];
 $num3 = $_GET['num3'];
 
 if(!isset($num1) || !isset($num2) || !isset($num3)) {
-	die("Vous devez appeler le script de cette facon : <br /><br /><strong>http://localhost/formation/PHP/exercices/conditions/exercice4.php?num1=4&num2=3&num3=12</strong>");
+	die("Vous devez appeler le script de cette facon : <br /><br /><strong>http://localhost/php/conditions/exercice4.php?num1=4&num2=3&num3=12</strong>");
 }
-
-/*
-* Completez le code ici
-*/
-
+// Mon code
+echo max($num1, $num2, $num3) . ' est le nombre le plus grand.';
