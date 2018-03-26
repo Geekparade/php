c56290caaa2da8081a9060dca029421ce8c40b11
Geekparade <david_da_silva0hotmail.fr>
2018-03-26 14:51:32 +0200

    déplacement des corrections dans le dossier Correction

--- boucles/exercice9.php
+++ boucles/exercice9.php
@@ -1,7 +1,47 @@
-<?PHP
+<h1>Creer un script permettant de créer plusieurs rectangles de tailles différentes et placer y des ronds à l'interieur de couleur differentes.</h1>
+<h2>Creer un formulaire demandant le nombre de carre ainsi que le nombre de rond par rectangle.</h2><br/>
 
-/**
-* Creer un script permettant de creer plusieurs rectangles de tailles differentes
-* et placer y des ronds a l'interieur de couleur differentes.
-* Creer un formulaire demandant le nombre de carre ainsi que le nombre de rond par rectangle.
-*/
+<?PHP
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Des carrés</title>
+<!-- script de style créer par Thomas Catinaud -->
+    <style>
+        .canvas {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+            align-items: center;
+            align-content: space-between;
+            margin: 20px;
+            min-height: 500px;
+            padding: 10px;
+            background: rgba(0,0,0,.1);
+        }
+        .square {
+            position: relative;
+            border: 1px solid pink;
+        }
+        .circle {
+            position: absolute;
+            border-radius: 50%;
+        }
+        .circle:nth-child(4n) {
+            background: FireBrick;
+        }
+        .circle:nth-child(4n+1) {
+            background: CornflowerBlue;
+        }
+        .circle:nth-child(4n+2) {
+            background: DarkGray;
+        }
+        .circle:nth-child(4n+3) {
+            background: DarkOrchid;
+        }
+    </style>
+</head>
--- conditions/exercice1.php
+++ conditions/exercice1.php
@@ -1,17 +1,29 @@
 <?php
-/**
-* Completez ce code source pour que le script affiche
-*
-*   $num1 est inferieur|superieur|egal a $num2
-*
-*/
+echo '<h1>Completez ce code source pour que le script affiche :';
+echo '<h3>$num1 est :</h3>';
+echo '<ul>
+		<li>inferieur a $num2</li>
+		<li>superieur a $num2</li>
+		<li>egal a $num2</li>
+	</ul>';
+
 $num1 = $_GET['num1'];
 $num2 = $_GET['num2'];
 if(!isset($num1) || !isset($num2)) {
 	die("Vous devez appeler le script de cette facon : <br /><br /><strong>http://localhost/formation/PHP/exercices/conditions/exercice1.php?num1=4&num2=3</strong>");
 }
 
-/*
-* Completez le code ici
-*/
+if ( $num1 < $num2 )
+{
+	echo $num1 . ' est inférieur à ' . $num2;
+} 
+else if ( $num1 > $num2 )
+{
+	echo $num1 . ' est supérieur à ' . $num2;
+}
+else
+{
+	echo $num1 . ' est égale à ' . $num2;
+}
+
 
