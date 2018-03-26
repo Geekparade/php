17db0093796c6aafd93d058a436c11a667ef2f63
Geekparade <david_da_silva0hotmail.fr>
2018-03-26 10:25:44 +0200

    m.a.j 2

--- boucles/exercice4.php
+++ boucles/exercice4.php
@@ -1,13 +1,28 @@
 <?php
+echo '<h1> Créer un script qui compte le nombre de voyelle
+ dans la variable $sentence </h1>' ;
 
-/**
-* Créer un script qui compte le nombre de voyelle
-* dans la variable $sentence
-*
-* 
-* indice on peut parcourir une chaine avec for
-* strlen()
-* in_array()
-*/
+echo '<h3>$sentence = "La formation simplon c\'est genial, on va devenir des pros !"</h3>' ;
 
-$sentence = "La formation simplon c'est genial, on va devenir des pros";
+echo '<p> Indice on peut parcourir une chaine avec :</p>
+	<li>	
+		for
+	</li>
+	<li>
+		strlen()
+	</li>
+	<li>	
+		in_array()
+	</li>' ;
+
+$sentence = "La formation simplon c'est genial, on va devenir des pros" ;
+$voyelle = ['a','e','i','o','u','y'] ;
+$nb_voyelle = 0 ;
+for ( $i = 0 ; $i < strlen($sentence) ; $i++) 
+{
+	if (in_array($sentence[$i], $voyelle)) 
+	{
+		$nb_voyelle++;
+	}
+};
+echo '<h2>Il y a '. $nb_voyelle . ' voyelles dans la phrase : \' ' . $sentence . ' \' .</h2>';
