d8eea46304e2d184c7182875c296848d979f6c78
Geekparade <david_da_silva0hotmail.fr>
2018-03-26 11:27:02 +0200

    boucle exo 6 finis

--- boucles/exercice6.php
+++ boucles/exercice6.php
@@ -1,6 +1,12 @@
+<h1>Ecrire un script qui calcul la somme des entiers de 1 a 20 et afficher le resultat .</h1>
+ <br/>
+<h3>Calcul de la somme des entiers de 1 a 20 :</h3>
 <?php
-
-/**
- * Ecrire un script qui calcul la somme des entiers de 1 a 20
- * et afficher le resultat
- */
+$total = 0 ;
+	for ( $i = 1 ; $i <= 20 ; $i++ )
+	{
+		echo '<li>' . $total += $i . '</li><br/>' ;
+	};
+	echo '<br/>';
+	echo 'La somme total est de ' . $total . '<br/>';
+?>
