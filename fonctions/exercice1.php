3094446bcb65712c9fbae039ddd6d5ab971486e7
Geekparade <david_da_silva0hotmail.fr>
2018-03-29 12:47:51 +0200

    fonction exo 1 fini

--- fonctions/exercice1.php
+++ fonctions/exercice1.php
@@ -1,8 +1,16 @@
 <?php
+// Créer une fonction qui teste si une valeur existe dans un tableau
+// Ne pas utiliser la fonction in_array
 
-/**
-* Créer une fonction qui teste si une valeur existe dans un tableau
-* 
-* ne pas utiliser la fonction in_array
-*/
+function inArray($array, $string)
+{
+	foreach ($array as $item ) 
+	{
+		if ($string == $item)
+		{
+			return true;
+		}
+	}
+	return false;
+}
 
